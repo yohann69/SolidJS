f5e0d3cd66aa40488e52c2ce925ec7b72081514a
Yohann <ychavanel@sm-adn.fr>
2023-04-17 13:58:03 +0200

    Improving code

--- Go-Routines/goroutines.go
+++ Go-Routines/goroutines.go
@@ -2,22 +2,48 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func block(s int) {
+func block(s int, wg *sync.WaitGroup, allDone *sync.WaitGroup) {
+	// defer wg.Done() // Est appelé à la fin de la fonction
+
+	// 1ere partie
 	start := time.Now()
 	time.Sleep(time.Second * time.Duration(s))
+
+	wg.Done()
+
+	allDone.Wait()
+
+	// 2ème partie
+	time.Sleep(time.Second * time.Duration(s))
+	wg.Done()
+
 	fmt.Printf("Durée d'éxecution: %s\n", time.Since(start))
+
 }
 
 func main() {
 
 	start := time.Now()
 
-	go block(5)
-	go block(1)
-	go block(3)
+	var wg sync.WaitGroup
+	var allDone sync.WaitGroup
+
+	allDone.Add(1)
+
+	wg.Add(3)
+
+	go block(5, &wg, &allDone)
+	go block(1, &wg, &allDone)
+	go block(3, &wg, &allDone)
+
+	wg.Wait()
+	wg.Add(3)
+	allDone.Done()
+	wg.Wait()
 
 	fmt.Printf("Durée d'éxecution: %s\n", time.Since(start))
 
